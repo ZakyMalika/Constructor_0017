8188862d5b5817c7af6ea4acf8319b9e64cf5f40
ZakyMalika <zakymalika0987@gmail.com>
2024-05-14 19:15:52 +0700

    membuat set judul menggunakan this pointer dan bukunya sebagai objek

--- chainFungsion.cpp
+++ chainFungsion.cpp
@@ -5,4 +5,16 @@ using namespace std;
 class buku
 {
     string judul;
-}
+
+public:
+    buku setJudul(string judul)
+    {
+        this->judul = judul;
+        return *this;
+
+    }    
+    string getJudul()
+    {
+        return this->judul;
+    }
+} bukunya;
